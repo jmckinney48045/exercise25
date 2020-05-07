67843af8d4025015f9595678d51591a861f41852
Jessica <jmckinney48045@gmail.com>
2020-05-07 17:27:14 -0400

    initial push

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exercise25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence: ");
+            string sentence = Console.ReadLine();
+            // use the input string split var list = sentence.Split(" ")
+
+            print stentence(List);
+
+            keepGoing = Keepgoing();
+
+            //what is the keep going
+            
+
+                
+
+  
+                
+               
+        }
+    }
+}
+
+
